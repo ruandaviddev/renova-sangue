1d0be701e6d549cccbe1a21b2a18d63c4d333a3b
Ruan David <ruandaviddev@gmail.com>
2026-06-18 16:43:30 -0300

    upload files

---
+++ src/main/java/com/web/ufc/renova_sangue/RenovaSangueApplication.java
@@ -0,0 +1,13 @@
+package com.web.ufc.renova_sangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovaSangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovaSangueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/ufc/renova_sangue/RenovaSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.ufc.renova_sangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovaSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
