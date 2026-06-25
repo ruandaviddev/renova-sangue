b3a403c49973df268b18ddb3714705b475a8ffff
yuklins <152190788+yuklins@users.noreply.github.com>
2026-06-25 12:48:24 -0300

    Adesão do FRONT (html + css) e Alteração no TesteController.
    TesteController é o controller do index.html. Quando possível renomear o controller para IncexController.

--- src/main/java/com/web/ufc/renova_sangue/controller/TesteController.java
+++ src/main/java/com/web/ufc/renova_sangue/controller/TesteController.java
@@ -1,12 +1,29 @@
 package com.web.ufc.renova_sangue.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TesteController {
+
     @GetMapping("/teste")
-    public String teste() {
-        return "teste";
+    public String teste(Model model) {
+        
+        // 1. Enviando os dados do Evento
+        Map<String, String> evento = new HashMap<>();
+        evento.put("data", "30–31 Mai");
+        evento.put("horario", "08h – 13h");
+        model.addAttribute("evento", evento);
+
+        // 2. Enviando os dados do Estoque Crítico
+        Map<String, String> estoques = new HashMap<>();
+        estoques.put("tipos", "Tipos O− e B−");
+        model.addAttribute("estoques", estoques);
+
+        return "index";
     }
 }
